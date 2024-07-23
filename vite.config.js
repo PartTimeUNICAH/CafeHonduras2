d78b653e0e0689da9249eb6b4ebd0595e6225850
PartTimeUNICAH <157434031+PartTimeUNICAH@users.noreply.github.com>
2024-07-23 15:54:51 -0600

    Update del vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import handlebars from 'vite-plugin-handlebars';
 import handlerBarsContext from './variables.js';
 
 export default defineConfig({
-    base: "/CafeHonduras/",
+    base: "/CafeHonduras2/",
     appType: 'mpa',
     build: {
         rollupOptions: {
